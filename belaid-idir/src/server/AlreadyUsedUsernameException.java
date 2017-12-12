12f75dbf7f402267bc06462f03b7d378d6e78efd
Idir Lankri <lankri.idir@gmail.com>
2017-12-12 22:59:38 +0100

    Fix some spelling mistakes in comments

--- belaid-idir/src/server/AlreadyUsedUsernameException.java
+++ belaid-idir/src/server/AlreadyUsedUsernameException.java
@@ -1,3 +1,3 @@
 package server;
-//TENTATIVE DE CREATION D'UN NOM CLIENT DÉJA EXISTANT//
+//TENTATIVE DE CREATION D'UN NOM CLIENT DÉJÀ EXISTANT//
 class AlreadyUsedUsernameException extends Exception {}
