6efaddf3bfa207eb02a58d7449b74e318188aa0d
Idir Lankri <lankri.idir@gmail.com>
2017-12-13 00:26:33 +0100

    Complete comments of class Logger

--- belaid-idir/src/client/Logger.java
+++ belaid-idir/src/client/Logger.java
@@ -2,18 +2,23 @@ package client;
 
 import java.util.logging.*;
 
+/*
+ * A simple logger that outputs to a file 'client.log' the messages
+ * exchanged on the network by the client application.
+ */
 class Logger {
     private static final java.util.logging.Logger logger = setup();
 
     private Logger() {}
 
+    /* Initialize the logger.  */
     private static java.util.logging.Logger setup() {
         final Handler handler;
         java.util.logging.Logger logger = null;
 
         try {
             LogManager.getLogManager().reset();
-            handler = new FileHandler("client.log");
+            handler = new FileHandler("bar/client.log");
             handler.setFormatter(new SimpleFormatter());
             logger = java.util.logging.Logger.getLogger("client");
             logger.addHandler(handler);
@@ -23,7 +28,7 @@ class Logger {
         }
         return logger;
     }
-    //AJOUT DE L'INFORMATION SUR L'HESTORIQUE DE CONNEXION DU CLIENT//
+    //AJOUT DE L'INFORMATION SUR L'HISTORIQUE DE CONNEXION DU CLIENT//
     static void info(String msg) {
         logger.log(Level.INFO, msg);
     }
